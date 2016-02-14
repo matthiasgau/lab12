8001b23824d6f8b350b8622dc436ececc51ab8eb
Goetz Lehmann <goetz@tp1.uni-duesseldorf.de>
2016-02-14 12:51:35 +0100

    solution

--- nlsi.cxx
+++ nlsi.cxx
@@ -7,15 +7,16 @@ using namespace std;
 //-----------------------------------
 typedef complex<double> cmplx;
 //-----------------------------------
+void NLStep( cmplx* const psi,   const double dt, const int Nx);
 void init( cmplx* const psi0, const double eta, const double sigma, const double dx,
           const int Nx);
-
+void LinStep( cmplx* const psi, const double dx, const double dt, const int Nx);
 void writeToFile(const cmplx* const v, const string s, const double dx,
                  const int Nx, const double xmin);
 //-----------------------------------
 int main(){
 
-	const int Nx = 4000;
+	const int Nx = 400;
 	const double L = 800;
 	const double xmin = 0;
 	const double Tend = 50;
@@ -34,16 +35,22 @@ int main(){
 
 	writeToFile(psi0,"psi_0", dx,Nx,xmin);
 
+	LinStep(psi0, dx, dt/2.0, Nx);
 
 	for (int i = 1; i <= Na; i++) {
-
+		LinStep(psi0, dx, dt/2.0, Nx);
+	        NLStep(psi0, dt, Nx);
 		for (int j = 1; j <= Nk-1; j++) {
+			LinStep(psi0, dx, dt, Nx);
+			NLStep(psi0, dt, Nx);
 		}
+		LinStep(psi0, dx, dt/2.0, Nx);
 		strm.str("");
 		strm << "psi_" << i;
 		writeToFile(psi0,strm.str(), dx,Nx,xmin);
 	}
 
+        delete[] psi0;
 	return 0;
 }
 //-----------------------------------
@@ -68,4 +75,37 @@ void init( cmplx* const psi0, const double eta,  const double dx, const double d
 		double x = i*dx - x0;
 		psi0[i] = 2*f/cosh(eta * x);
 	}
+}
+//-----------------------------------
+void NLStep( cmplx* const psi, const double dt, const int Nx){
+	for(int i=0; i<Nx; i++){
+		double phi = abs(psi[i]) * abs(psi[i]) * dt ;
+	    psi[i] *= cmplx(cos(phi), sin(phi));
+	}
+}
+//-----------------------------------
+void LinStep(cmplx* const rhs, const double dx, const double dt,
+						 const int Nx)
+{
+	cmplx* u =new cmplx[Nx];
+
+	cmplx alpha = cmplx(0.0, dt/(dx*dx));
+
+	cmplx d =  (cmplx(1) + cmplx(2.0,0) * alpha) / (-alpha);
+
+	//forward substitution
+	u[0] = cmplx(1) / d;
+	rhs[0] = rhs[0] / -alpha /d;
+
+	for(int i=1; i<Nx; i++){
+		u[i] = cmplx(1) / (d - u[i-1]);
+		rhs[i] = (rhs[i]/(-alpha) - rhs[i-1]) / (d - u[i-1]);
+	}
+
+
+	//backward substitution
+	for(int i=Nx-2; i>=0; i--)
+		rhs[i] = rhs[i] - u[i]*rhs[i+1];
+
+  delete[] u;
 }
